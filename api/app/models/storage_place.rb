ea5c625241245509bc50b314386a5dfad4cf36e8
Waris Ali <m.warisali2@gmail.com>
2021-08-19 16:19:25 +0500

    api: Add storage place resource

---
+++ api/app/models/storage_place.rb
@@ -0,0 +1,3 @@
+class StoragePlace < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :book_shelves
+  resources :boxes
   resources :topics
   resources :authors
 end
